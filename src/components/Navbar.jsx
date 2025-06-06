c5d67cacc3c2dbc0a166866ad1e7c4f18894df26
Md Zafar <mz5659875@gmail.com>
2025-06-06 12:42:36 +0530

    Navbar UI Fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,39 @@
 import { useEffect, useState } from 'react';
+import { CiSearch, CiLogin } from "react-icons/ci";
+import { IoMdArrowDropdown } from "react-icons/io";
 import '../css/Navbar.css'
 
-const Navbar = ()=> {
+const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
-  useEffect(()=>{
-    const handleScroll = ()=>{
-      setIsScrolled(window.scrollY>0);
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
     }
 
     window.addEventListener('scroll', handleScroll);
-  },[isScrolled]);
+  }, [isScrolled]);
 
   return (
-    <nav className={`navbar ${isScrolled ? 'shadow':''}`}>
+    <nav className={`navbar ${isScrolled ? 'shadow' : ''}`}>
       <ul className="navbarLinks">
-        <li><a href="/">Search</a></li>
-        <li><a href="/about">About</a></li>
+        <li>
+          {<CiSearch />}
+          <a href="/">Search</a>
+        </li>
+        <li className='about-dropdown'>
+          <a href="#">About</a>
+          {<IoMdArrowDropdown className='about-dropdown-icon'/>}
+          <ul className='dropdown-menu'>
+            <li><a href="/about/mission">Mission</a></li>
+            <li><a href="/about/partners">Partners</a></li>
+          </ul>
+        </li>
         <li><a href="/">Campagin Fund</a></li>
-        <li><a href="/signin">Sign in</a></li>
+        <li>
+          <a href="/signin">Sign in</a>
+          {<CiLogin />}
+        </li>
         <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
