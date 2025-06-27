ee295677e09a628585c834b17638fb8451fea3d3
ShaluKasera <shalukumari93129@gmail.com>
2025-06-27 13:26:13 +0530

    add axios.js

---
+++ src/utils/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Auto logout
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
+
